24ac7f5191511b73f8cfd9acef8d62f51891a2c4
fsiddh <jasonshaw546@gmail.com>
2020-09-14 08:34:59 +0530

    update 1.2

--- NoBroker/NoBroker/spiders/NBspider.py
+++ NoBroker/NoBroker/spiders/NBspider.py
@@ -1,4 +1,6 @@
 import scrapy
+import pandas as pd
+
 
 class NBSpider(scrapy.Spider):
     name = 'nb_spider'
@@ -6,9 +8,32 @@ class NBSpider(scrapy.Spider):
         'https://www.nobroker.in/property/rent/mumbai/Navi%20Mumbai/?searchParam=W3sibGF0IjoxOS4wMzMwNDg4LCJsb24iOjczLjAyOTY2MjUsInBsYWNlSWQiOiJDaElKclJNZnVQQzU1enNSYWZpRkVXajNFanciLCJwbGFjZU5hbWUiOiJOYXZpIE11bWJhaSJ9XQ==&sharedAccomodation=0&orderBy=nbRank,desc&radius=2&traffic=true&travelTime=30&propertyType=rent&pageNo=10'
     ]
 
-    def parse(self, response):
-        area = response.css('.solid-border-right meta+ span::text').extract()
-        price = response.css('.solid-border-right .inr_resale+ span::text').extract()
+
+    def __init__(self):
+            self.items = {
+                'title':[],
+                'price':[],
+                'area':[],
+                # 'owner_name':[],
+                'address':[]
+            }
+
+
+    def parse(self, response):              
         title = response.css('h2::text').extract()
+        price = response.css('.solid-border-right .inr_resale+ span::text').extract()
+        area = response.css('.solid-border-right meta+ span::text').extract()
         # owner_name = response.css('.seller-name span::text').extract()
         address = response.css('.card-header-title h5::text').extract()
+
+        self.items['title'].append(title)
+        self.items['price'].append(price)
+        self.items['area'].append(area)
+        self.items['address'].append(address)
+
+        df = pd.DataFrame.from_dict(scraped_info, orient='index')
+		# df.transpose()	
+		df.head()
+		df.to_excel("navi_mumbai.xlsx")
+
+		yield self.items
